3c6dad416c221c7b97ed1b2583de8d9e14e206ae
sarria <juan@townsquaredigital.com>
2022-03-30 21:39:56 -0400

    Fixing issues to deploy - 1

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,6 @@
 import { ReactElement } from 'react';
 import Image from 'next/image'
-// import Burger from './burger'
-// import Quote from "./Quote"
-// // import parse from 'html-react-parser';
-// import Link from 'next/link'
-// import classNames from 'classnames/bind'
 import styles from './styles/hero.module.scss'
-// import logo from '../public/PSLogo.png'
-// import decoration from '../public/decoration.png'
-// import mask from '../public/mask.png'
-// import Script from 'next/script'
-// // import SimpleImageSlider from "react-simple-image-slider"; 
 
 function Hero({ title, headerImage }: { title: string, headerImage: {altText: string, sourceUrl: string} }): ReactElement {
 	return (
