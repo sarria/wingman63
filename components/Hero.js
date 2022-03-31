85c54ac43c50ee1f95fe645684037a7afcaafb89
sarria <juan@townsquaredigital.com>
2022-03-30 21:47:53 -0400

    Fixing issues to deploy - 2

--- components/Hero.js
+++ components/Hero.js
@@ -1,20 +1,11 @@
 import { ReactElement } from 'react';
 import Image from 'next/image'
-// import Burger from './burger'
-// import Quote from "./Quote"
-// // import parse from 'html-react-parser';
-// import Link from 'next/link'
-// import classNames from 'classnames/bind'
 import styles from './styles/hero.module.scss'
-// import logo from '../public/PSLogo.png'
-// import decoration from '../public/decoration.png'
-// import mask from '../public/mask.png'
-// import Script from 'next/script'
-// // import SimpleImageSlider from "react-simple-image-slider"; 
 
-function Hero({ title, headerImage }: { title: string, headerImage: {altText: string, sourceUrl: string} }): ReactElement {
+function Hero({ title, headerImage }) {
 	return (
 		<div className={styles.root}>
+			ok
 			<div className={styles.background}>
 				<div className={styles.left}></div>
 				<div className={styles.right}></div>
