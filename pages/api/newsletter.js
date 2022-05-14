98213aa1fcd3bab85fdba5732e92bdeceb91dd08
sarria <juan@townsquaredigital.com>
2022-05-13 21:00:20 -0400

    Adjusting contact us message error

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -82,7 +82,7 @@ const Newsletter = async (req, res) => {
     return res.status(201).json({ error: null });
   } catch (error) {
     return res.status(400).json({
-      error: `Ops!, something went wrong...`,
+      error: `Ops!, something went wrong... Send me an email at info@wingman63.com and I'll add you to the list.`,
     });
 
     // Report error to Sentry or whatever
