9d78680276eacb57944217a8b2ef8214e0964250
JuanCerbo <120061769+JuanCerbo@users.noreply.github.com>
2023-08-17 08:35:14 -0700

    Juas is testing with Scott

--- components/Footer.js
+++ components/Footer.js
@@ -131,7 +131,7 @@ const Footer = ({global, buttons}) => {
 							</div>
 						</div>
 						<div className={cx(styles.copyRight, styles.mobile)}>
-							{parse(global.copyRight)}
+							{parse(global.copyRight)} 2023
 						</div>						
 					</div>
 				</div>
