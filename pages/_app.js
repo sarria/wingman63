1ca49eb8903bf18f92a2edd97c29e1baada082da
sarria <juan@townsquaredigital.com>
2024-06-11 12:19:52 -0400

    Removing typescript

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,7 @@
 import '../styles/globals.css'
 import '../styles/burger.scss'
-import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
